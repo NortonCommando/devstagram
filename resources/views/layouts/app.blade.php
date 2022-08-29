1dab792ba7e17a3f586151701ffc84983652d029
kepa <kepa.mola@gmail.com>
2022-08-29 11:01:08 +0200

    Dropzone configuracion basica

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    @stack('styles')
     <title>DevStagram - @yield('title')</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 </head>
 
 <body class="bg-gray-100">
--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 Route::get('/post/create', [PostController::class, 'create'])->name('posts.create');
+
+Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
