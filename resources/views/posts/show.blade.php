b5b2c4889b3fcfe237eb3ffdca1ea6a93dc0380d
Kepa <kepa.mola@gmail.com>
2022-09-04 10:59:08 +0200

    Maquetacion y helper de fechas

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,4 +5,20 @@
 @endsection
 
 @section('contenido')
+    <div class="container mx-auto flex">
+        <div class="md:w-1/2">
+            <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}" />
+            <div class="p-3">
+                <p>0 likes</p>
+            </div>
+            <p class="font-bold italic">{{ $post->user->username }} </p>
+            <p class="text-sm text-gray-500">
+                {{ $post->created_at->diffForHumans() }}
+            </p>
+            <p class="mt-5">{{ $post->descripcion }}</p>
+        </div>
+        <div class="md:w-1/2">
+        </div>
+
+    </div>
 @endsection
