3268d10877286bfc69cfad9894c21235b1de25a5
Kepa <kepa.mola@gmail.com>
2022-12-08 11:31:06 +0100

    Implementacion livewire y like post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,22 +10,12 @@
             <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}" />
             <div class="p-3 flex items-center gap-4">
                 @auth
-                    @php
-                        $mensaje = 'Hola mundo desde una variables sd as';
-                    @endphp
                     <livewire:like-post :post="$post" />
-                    @if (!$post->checkLike(auth()->user()))
+                    {{-- @if (!$post->checkLike(auth()->user()))
                         <form method="POST" action="{{ route('posts.likes.store', $post) }}">
                             @csrf
                             <div class="my-4">
-                                <button type="submit">
-                                    <svg class="w-6 h-6" fill="white" stroke="currentColor" viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
-                                        </path>
-                                    </svg>
-                                </button>
+
                             </div>
                         </form>
                     @else
@@ -43,7 +33,7 @@
                                 </button>
                             </div>
                         </form>
-                    @endif
+                    @endif --}}
 
                 @endauth
                 <p>{{ $post->likes->count() }} likes</p>
