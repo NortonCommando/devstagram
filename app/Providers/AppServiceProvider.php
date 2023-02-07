256abf62bf7658ba2e52b48371daff27d769b3c5
Kepa <kepa.mola@gmail.com>
2023-02-07 09:56:13 +0100

    No https here

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
     }
 }
